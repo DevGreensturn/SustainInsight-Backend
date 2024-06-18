95f7d7fd30eb099cd2575b01156d3e2c24cc38c0
pradeepGreen00 <dev2@greensturn.com>
2024-06-18 15:15:40 +0530

    delete

--- routes/v1/suppliers.js
+++ routes/v1/suppliers.js
@@ -6,6 +6,7 @@ const supplier = require('../../controller/supplierController');
 
 router.post('/',auth.authenticateToken,supplier.createSupplier)
 router.put('/:id',auth.authenticateToken,supplier.updateSupplier);
-router.get('/:id?',supplier.getSupplier) 
+router.get('/:id?',supplier.getSupplier); 
+router.patch('/:id',auth.authenticateToken,supplier.deleteSupplier);
 
 module.exports = router;
